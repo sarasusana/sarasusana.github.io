00b12bac27b0f8968a32235901a2bcd7fd6e9e6f
Sara Campoverde de Burgos <sasucamp@hotmail.com>
2018-06-28 16:29:53 -0500

    modularization

---
+++ assignments/weathersite/_footer.php
@@ -0,0 +1,58 @@
+    </main>
+      <br>
+  <footer class="footer-box">
+           
+      <section class="box-1">
+      <h3>Address:</h3>
+      <p>The Weather Center</p>
+      <p>123 Main Street</p>
+      <p>Franklin, MI 87654</p>
+      </section>
+      
+      <section class="box-2">
+      <h3>Phone:</h3>
+      <p>(801) 555-1234</p>
+      </section>
+      
+      <section class="box-2">
+      <h3>Email</h3>
+      <p>contact@youurl.com</p>
+      <p></p>
+      </section>
+          
+      <p id="box-3">&#169; 2018 &#8226; All right reserved <br>
+          <span id="currentDate"></span></p>
+
+    </footer>
+
+   
+</body>
+    
+    <script src="js/currentdate.js"></script>
+    <script src="js/hamburger.js"></script>
+    <script src="js/windchill.js"></script>
+    <script src="js/towndata.js"></script>
+    
+    <script>
+    
+        var urlString = document.location.href;
+        var urlArray = urlString.split('/');
+        var pageHREF=urlArray[urlArray.length-1];
+        
+        if (pageHREF !=="") {
+            var menu = document.querySelectorAll('ul#primaryNav li a');
+            var i;
+            //loop through all the menu items in the navigation
+            for(i=0; i<menu.length; i++) {
+                var currentURL=(menu[i].getAttribute("href"));
+                menu[i].parentNode.className=""; //turn off highlight by default
+                if (currentURL===pageHREF) {
+                    menu[i].parentNode.className="active"; //turn on if a match
+                } //end if
+            } //end of the search for a match
+        } //end of if !==
+        
+    </script>
+    
+    
+</html>
---
+++ assignments/weathersite/_header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head> 
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Allerta|Raleway" rel="stylesheet">
+        <meta name="description" content="Welcome to How's the Weather, you realiable and accurate site to know the weather report">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/small.css">
+        <link rel="stylesheet" href="css/medium.css">
+        <link rel="stylesheet" href="css/large.css"> 
+        <title>How's the Weather</title>
+</head>
+    
+<body>
+  <header class="clearfix"><a href="index.html">
+  <figure class="logo">
+      <img src="images/logo0.png" alt="logo" srcset="images/logo1.png 1x, images/logo2.png 2x, images/logo3.png 3x">
+      </figure></a>
+  <h1>How's the Weather</h1>
+  <h2 class="subheading">Stay up to date</h2>
+  </header>
+
+  <nav class="clearfix">
+      <button onclick="toggleMenu()">&#9776;</button>
+    <ul class="hide" id="primaryNav">
+    	<li class="active"><a href="index.html">Home</a></li>
+    	<li><a href="franklin.html">Franklin</a></li>
+    	<li><a href="#">Greenville</a></li>
+    	<li><a href="#">Springfield</a></li>
+    	<li><a href="stormcenter.html">Storm Center</a></li>
+    	<li><a href="gallery.html">Gallery</a></li>
+    </ul>		
+  </nav>
+    
+<main>
