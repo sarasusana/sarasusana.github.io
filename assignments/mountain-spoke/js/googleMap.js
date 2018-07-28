e5d54a03dbd19ca6b8d4ac83e68329ed52d1e1e9
Sara Campoverde de Burgos <sasucamp@hotmail.com>
2018-07-28 09:27:08 -0500

    google map

---
+++ assignments/mountain-spoke/js/googleMap.js
@@ -0,0 +1,7 @@
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 45.672389, lng:  -111.090750},
+          zoom: 8
+        });
+      }
