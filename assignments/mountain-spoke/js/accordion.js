ee5fedf4a6ad74608a6fd099c27af49e3bbef3e0
Sara Campoverde de Burgos <sasucamp@hotmail.com>
2018-07-28 09:00:17 -0500

    added mountain spoke page

---
+++ assignments/mountain-spoke/js/accordion.js
@@ -0,0 +1,13 @@
+function openDay(evt, dayName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(dayName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
