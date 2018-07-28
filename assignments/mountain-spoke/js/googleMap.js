ee41c5a037215ac85d7b13a49ba4435c306c4ea5
Sara Campoverde de Burgos <sasucamp@hotmail.com>
2018-07-28 18:43:22 -0500

    edited google map code

--- assignments/mountain-spoke/js/googleMap.js
+++ assignments/mountain-spoke/js/googleMap.js
@@ -1,7 +1,31 @@
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 45.672389, lng:  -111.090750},
-          zoom: 15
-        });
-      }
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+
+function initialize() {
+  var bozeman = { lat: 45.6724013, lng: -111.0912754 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: bozeman
+  });
+
+  // This event listener calls addMarker() when the map is clicked.
+  google.maps.event.addListener(map, 'click', function(event) {
+    addMarker(event.latLng, map);
+  });
+
+  // Add a marker at the center of the map.
+  addMarker(bozeman, map);
+}
+
+// Adds a marker to the map.
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  var marker = new google.maps.Marker({
+    position: location,
+    label: labels[labelIndex++ % labels.length],
+    map: map
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
