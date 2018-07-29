59598a863d548efdb49305b2338714af0d793132
Sara Campoverde de Burgos <sasucamp@hotmail.com>
2018-07-28 19:22:29 -0500

    added jquery

--- assignments/mountain-spoke/js/googleMap.js
+++ assignments/mountain-spoke/js/googleMap.js
@@ -1,31 +1,7 @@
-var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var labelIndex = 0;
-
-function initialize() {
-  var bozeman = { lat: 45.6724013, lng: -111.0912754 };
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: bozeman
-  });
-
-  // This event listener calls addMarker() when the map is clicked.
-  google.maps.event.addListener(map, 'click', function(event) {
-    addMarker(event.latLng, map);
-  });
-
-  // Add a marker at the center of the map.
-  addMarker(bozeman, map);
-}
-
-// Adds a marker to the map.
-function addMarker(location, map) {
-  // Add the marker at the clicked location, and add the next-available label
-  // from the array of alphabetical characters.
-  var marker = new google.maps.Marker({
-    position: location,
-    label: labels[labelIndex++ % labels.length],
-    map: map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 45.6724254, lng: -111.0914935},
+    zoom: 17
   });
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
