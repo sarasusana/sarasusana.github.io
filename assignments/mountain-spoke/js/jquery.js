05cb2cd33dc581c224c638b7618119ddc70bb8b6
Sara Campoverde de Burgos <sasucamp@hotmail.com>
2018-07-29 00:00:06 -0500

    mountain page

---
+++ assignments/mountain-spoke/js/jquery.js
@@ -0,0 +1,41 @@
+//----------sales page-----------
+$(document).ready(function() {
+
+  $('a.panel').click(function() {
+
+    $('a.panel').removeClass('selected');
+    $(this).addClass('selected');
+
+    current = $(this);
+
+    $('#wrapper').scrollTo($(this).attr('href'), 800);
+
+    return false;
+  });
+
+  $(window).resize(function() {
+    resizePanel();
+  });
+
+});
+
+function resizePanel() {
+
+  width = $(window).width();
+  height = $(window).height();
+
+  mask_width = width * $('.item').length;
+
+  $('#debug').html(width + ' ' + height + ' ' + mask_width);
+
+  $('#wrapper, .item').css({
+    width: width,
+    height: height
+  });
+  $('#mask').css({
+    width: mask_width,
+    height: height
+  });
+  $('#wrapper').scrollTo($('a.selected').attr('href'), 0);
+
+}
